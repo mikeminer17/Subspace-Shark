7d8ae45b9953ed9ab8d636821173125a3ba6d8e1
mikeminer17 <103860806+mikeminer17@users.noreply.github.com>
2022-06-10 18:32:34 +0300

    Reverted version to june-05

--- subspace.sh
+++ subspace.sh
@@ -17,8 +17,8 @@ sleep 1 && curl -s https://raw.githubusercontent.com/f5nodes/logo/main/logo-shar
 
 cd $HOME
 rm -rf subspace*
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-08/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-08
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-08/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-08
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-05
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-05
 chmod +x subspace*
 mv subspace* /usr/local/bin/
 
